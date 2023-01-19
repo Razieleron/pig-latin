8a03cef05614244860d0c4fe6f64dcde50266914
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-01-19 15:51:01 -0800

    add pseudocode logic / avenues to complete the pig latin language modifier

--- pig latin.md
+++ pig latin.md
@@ -1,24 +1,24 @@
 Describe: pigLatin();
 
-Test:"It will add 'way' to the end of words that begin with a vowel."
-Code:pigLatin("a");
-Expected Output: "away"
+<!-- Test:"It will add 'way' to the end of words that begin with a vowel."
+Code:pigLatin("a", "apple");
+Expected Output: "away" "appleway"
 
 Test:"It will add 'consonant'-ay to the end of consonants."
 Code:pigLatin("b");
-Expected Output: "bay"
+Expected Output: "bay" -->
 
 Test: "It will only apply 'ay' to the first letter in a word."
-Code: pigLatin("ba");
-Expected Output: "baya"
+Code: pigLatin("bat");
+Expected Output: "bayat"
 
-Test: "It will correctly understand how to conjugate words that start with 'qu'."
+Test: "For words that start with 'Qu', it will add 'ay' after the 'u' and not the 'q'."
 Code: pigLatin("quilt")
-Expected Output: "iltquay"
+Expected Output: "quayilt" 
 
-Test:"It will move the first character with 'ay' appended to the end of the string."
-Code:pigLatin("ba");
-Expected Output: "abay"
+Test:"It will move the first character(or two characters 'qu') with 'ay' appended to the end of the string."
+Code:pigLatin("bat");
+Expected Output: "atbay"
 
 Test: "It will apply the 'ay' appending and consonant/vowel moving to the end of multiple words in a string"
 Code: pigLatin("me too");
---
+++ piglatin.js
@@ -0,0 +1,97 @@
+function pigLatin(letter) {
+  let vowelPigLatinizer = "way";
+  return letter + vowelPigLatinizer;
+}
+
+function pigLatin(letter) {
+  let vowelPigLatinizer = "way";
+  let consonantPigLatinizer = "ay";
+  if letter = ["a", "e", "i", "o", "u"]
+    return letter + vowelPigLatinizer;
+  else return letter + consonantPigLatinizer;
+}
+
+function pigLatin(letter) {
+  let vowelPigLatinizer = "way";
+  let consonantPigLatinizer = "ay";
+  let quPigLatinizer = "ay"
+  if letter = ["a", "e", "i", "o", "u"]
+  return letter + vowelPigLatinizer;
+  if letter = ["qu"]
+  return letter + quPigLatinizer;
+  if letter = [any consonant]
+  return letter + consonantPigLatinizer;
+}
+
+function pigLatin(word) {
+  let wordArray = word.split()
+  // wordArray = [w, o, r, d]
+  wordArray[0] = wordArray[0] + "ay"
+  // wordArray = [way, o, r, d]
+  let conjugatedWord = wordArray.join("")
+  return conjugatedWord
+  // conjugatedWord = "wayord"
+
+function pigLatin(quilt) {
+  let wordArray = quilt.split("")
+  // wordArray = [q, u, i, l, t] 
+  if wordArray[0] = (a, e, i, o, u)
+
+    for (let wordArray[wordArray.length] = way)
+
+    then wordArray[0] = wordArray[0] + vowelPigLatinizer
+  if wordArray[0] = (q)
+    then wordArray[0] = wordArray[1] + quPigLatinizer
+  // wordArray = [q, uay, i, l, t]
+  else, wordArray[0] = wordArray[0] + consonantPigLatinizer
+
+  let conjugatedWord = wordArray.join("")
+  return conjugatedWord:
+  // conjugatedWord = quayilt
+}
+
+wordArray = word.split
+// wordArray = [w, o, r, d]
+do thing
+// add ay to w so wordArray = [way, o, r, d]
+etherealWordArray = []
+etherealWordArray.push(wordArray[1])
+etherealWordArray.push(wordArray[2])
+etherealWordArray.push(wordArray[3])
+etherealWordArray.push(wordArray[0])
+
+etherealWordArray = [o, r, d, way]
+ConjugatedWord = etherealWordArray.join("");
+ConjugatedWord = "ordway"
+
+
+function pigLatin(me too buddy)
+  let sentenceArray = me too buddy.split(" ")
+  // sentenceArray = ["me", "too", "buddy"]
+  let word1Array = sentenceArray[0].split("")
+  // word1Array = [m, e]
+  let word2Array = sentenceArray[1].split("")
+  // word2Array = [t, o, o]
+  let word3Array = sentenceArray[2].split("")
+  // word3Array = [b, u, d, d, y]
+}
+
+if there is a way where we can separate each character
+
+function pigLatin (word) {
+	word.slice(0,0)
+  
+  if index[0] = "q"
+  quick.slice(0,0) + quick.slice(0,1).concat("quay")
+  if index[0] = (a, e, i, o, u)
+  eye.concat("way")
+  else word.slice(0,0)
+  return word
+  ickqu as output
+}
+
+let finishedWord = word.slice(0,0).concat("any consonant + ay")
+
+somebody type something
+
+
