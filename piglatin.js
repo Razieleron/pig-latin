b15b87a430315e1dc7c31d0f2b1080e607959034
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-01-19 17:01:05 -0800

    included ChatGPT code

--- piglatin.js
+++ piglatin.js
@@ -95,3 +95,85 @@ let finishedWord = word.slice(0,0).concat("any consonant + ay")
 somebody type something
 
 
+:::::EXERCISE::::
+
+have a string of x length, say 'macaroni'
+move index[0] to the end of the Array
+
+
+function pigLatin(word)
+for (let i= 0; i < word.length; i++) {
+  if (word[i].startsWith("qu"){
+    word[i].slice(0,2);
+  });
+}
+// so what is word = ? quick -> ick
+
+function pigLatin(word)
+for (let i= 0; i < word.length; i++) {
+  if (word[i].startsWith("a", "e", "i", "o", "u"){
+    word.concat("way");
+  });
+
+function pigLatin(word)
+for (let i= 0; i < word.length; i++) {
+  if (word[i].startsWith(!"qu", !"a", !"e", !"i", !"o", !"u")) {
+    word[i].slice(0,1);
+  }
+}
+
+
+
+
+
+
+
+
+function convertToPigLatin(sentence) {
+  let words = sentence.split(" ");
+  let pigLatinWords = words.map(function(word) {
+    let firstLetter = word[0];
+    return word.slice(1) + firstLetter + "ay";
+  });
+  return pigLatinWords.join(" ");
+}
+
+
+function convertToPigLatin(sentence) {
+  let words = sentence.split(" ");
+  let pigLatinWords = words.map(function(word) {
+    if(/^[aeiou]/i.test(word)) return word+"way"; // start with vowels
+    if(/^qu/i.test(word)) return word.slice(2) + "quay"; // start with qu
+    let firstLetter = word[0];
+    return word.slice(1) + firstLetter + "ay";
+  });
+  return pigLatinWords.join(" ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
